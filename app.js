bbccea80a5a22826ea28fa5d4018c38f5b0519fa
Philip Yap <philipyap@Philips-MacBook.local>
2020-07-16 15:28:41 -0700

    [calculator]add num

--- app.js
+++ app.js
@@ -1,8 +1,58 @@
-function greeting(name) {
-    let sentence = 'Hello, my name is ' + name;
+function greeting(name, callback) {
+    let sentence = 'Hello, my name is ' + name + ' ';
 
+    let otherSentence = callback();
+
+    return sentence + otherSentence;
+}
+
+
+function anotherGreeting() {
+    let sentence = 'I forgot my last name';
+    
     return sentence;
 }
 
 let myName = 'Philip';
-console.log(greeting(myName));
+console.log(greeting(myName, anotherGreeting));
+
+function printArray(array, callback) {
+    for(let i = 0; i < array.length; i++) {
+        let numbers = array[i]; // 1, 2 ..
+
+        console.log(number); // 1
+    }
+
+    callback(array);
+}
+
+function printArrayAgain(array) {
+    for (let i = 0; i < array.length; i++) {
+        let number = array[i]; //1,2 ..
+
+        console.log(number);//1,2 ..
+    }
+
+}
+
+const numbersArray = [1,2,3,4,9,9,7,6];
+
+printArray(numbersArray, printArrayAgain);
+
+function calculator(num1, num2, callbackOne, callbackTwo) {
+    let result = callbackOne(num1, num2) + callbackTwo(num1, num2);
+
+    return result;
+} 
+
+function addNumbers(num1,num2) {
+    for(let i = 0, i < num1.length; i++) {
+        let 
+    }
+}
+
+function subtractNumber(num1, num2) {
+
+}
+
+console.log(calculator(5,77, addNumbers, subtractNumbers));
